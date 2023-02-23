97e0dbfa8f038c74aa423e32bf9b2af32f30736a
Caroline <clri@bu.edu>
2023-02-22 20:56:24 -0500

    Add main.js to read and solve a grid
    Take the grid as a JSON file and parse it. Assume validity.
    
    Solves the grid if it can, and outputs the best route if so.

---
+++ main.js
@@ -0,0 +1,24 @@
+const gridUtils = require("./grid");
+const fs = require('fs');
+
+if (process.argv.length < 3) {
+  throw new Error("Please input a filename");
+}
+
+let filename = process.argv[2];
+let gridString = fs.readFileSync(filename, 'utf8');
+
+
+let gridArray = JSON.parse(gridString);
+let gridObj = {
+  Grid: gridArray,
+  BestRoute: undefined
+};
+
+if (gridUtils.isSolvable(gridObj)) {
+  console.log("This grid is solvable. Most efficient route:");
+  gridUtils.printRoute(gridObj.BestRoute.Grid);
+}
+else {
+  console.log("This grid is note solvable");
+}
